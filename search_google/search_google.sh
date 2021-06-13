b3baa59c2ad973e1de80711208df8e33fcec0a9e
Jimmie Roos <jimmieroos93@gmail.com>
2021-06-13 12:48:31 +0200

    added relative path support for helper script

--- search_google/search_google.sh
+++ search_google/search_google.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
+exe_dir=$(dirname "$0")
+
 # Copy selected text to clipboard.
 a=$(xclip -o -selection primary)
 
 # Start a python script that uses selenium to open a browser and paste the
 # copied text into google.
-./helper.py $a 
+$exe_dir/helper.py $a 
